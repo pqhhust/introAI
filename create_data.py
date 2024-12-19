956a066958b2ab41628c19adb1e4cc51063effb7
HoangTran223 <tranvuonghoang204@gmail.com>
2024-12-19 21:03:07 +0700

    create data

---
+++ create_data.py
@@ -0,0 +1,11 @@
+﻿import osmnx as ox
+
+north, south, east, west = 21.04299, 21.03327, 105.82080, 105.81277
+bbox = (north, south, east, west)
+
+G = ox.graph.graph_from_bbox(bbox, network_type='all', retain_all=True, simplify=False )
+
+ox.save_graphml(G, filepath='lieu_giai_graph_detailed.graphml')
+
+print("Đã tạo file lieu_giai_graph_detailed.graphml")
+
---
+++ merge_data.py
@@ -0,0 +1,21 @@
+﻿import networkx as nx
+
+file1 = "D:\introAI\lieu_giai_graph.graphml"
+file2 = "D:\introAI\lieu_giai_graph_detailed.graphml"
+output_file = "D:\introAI\lieu_giai_graph_merged.graphml"
+
+G1 = nx.read_graphml(file1)
+G2 = nx.read_graphml(file2)
+
+for node, data in G2.nodes(data=True):
+    if node not in G1:  
+        G1.add_node(node, **data)
+
+for edge in G2.edges(data=True):
+    if not G1.has_edge(edge[0], edge[1]): 
+        G1.add_edge(edge[0], edge[1], **edge[2])
+
+
+nx.write_graphml(G1, output_file)
+
+print(f"File hợp nhất lưu tại: {output_file}")
